1fa797f7024d536bdfb808748f49669b214d4bd8
Ivaylo Yankov <mayallzobject@gmail.com>
2024-06-26 17:39:18 +0300

    feature: app layout

---
+++ src/layouts/appLayout.tsx
@@ -0,0 +1,14 @@
+// import NavBar from '../components/navigation/navBar';
+// import AppContent from '../components/content/content';
+
+const AppLayout = () => {
+  return (
+    <div id="app">
+      App Layout
+      {/* <AppContent />
+      <NavBar /> */}
+    </div>
+  );
+};
+
+export default AppLayout;
---
+++ src/routes.tsx
@@ -0,0 +1,34 @@
+import { RouteObject } from 'react-router-dom';
+import AppLayout from './layouts/appLayout';
+
+// todo: add error element
+export const publicRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <>Error</>,
+    children: [
+      {
+        index: true,
+        async lazy() {
+          const { HomePage } = await import('./pages/home');
+          return { Component: HomePage };
+        },
+      },
+      {
+        path: 'boost',
+        async lazy() {
+          const { BoostPage } = await import('./pages/boost');
+          return { Component: BoostPage };
+        },
+      },
+      {
+        path: 'earn',
+        async lazy() {
+          const { EarnPage } = await import('./pages/earn');
+          return { Component: EarnPage };
+        },
+      },
+    ],
+  },
+];
