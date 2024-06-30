8e8c8bdc5cd7c6b1ef2d080fcee1fc0105d85e82
Ivaylo Yankov <mayallzobject@gmail.com>
2024-06-30 18:23:42 +0300

    fix: build

--- src/components/testFirebase.tsx
+++ src/components/testFirebase.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const FirebaseConfigTest = () => {
   const [config, setConfig] = useState(null);
