063c888c0f4fd579fdb0789fb5d710df6abf9c4c
Ivaylo Yankov <mayallzobject@gmail.com>
2024-06-30 20:02:54 +0300

    test: home button

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useUser } from './context/useUser';
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+import { publicRoutes } from './routes';
 
-const App: React.FC = () => {
-  const { userId, userData, updateScore, error } = useUser();
-
-  // Example function to handle some user action that updates the score
-  const handleScoreUpdate = () => {
-    if (userData) {
-      const newScore = userData.score_points + 10; // For example, increment score by 10
-      updateScore(newScore);
-    }
-  };
-
-  return (
-    <div>
-      <h1>User Data {userId}</h1>
-      {error && <p>{error}</p>}
-      {userData ? (
-        <>
-          <pre>{JSON.stringify(userData, null, 2)}</pre>
-          <button onClick={handleScoreUpdate}>Update Score</button>
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+const App = () => {
+  const router = createBrowserRouter(publicRoutes);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,35 @@
+import { useUser } from '../context/useUser';
+
 const HomePage = () => {
+  const { userId, userData, updateScore, error } = useUser();
+
+  // Example function to handle some user action that updates the score
+  const handleScoreUpdate = () => {
+    if (userData) {
+      const newScore = userData.score_points + 10; // For example, increment score by 10
+      updateScore(newScore);
+    }
+  };
+
   return (
-    <h1>
-      <button>Press for Coins</button>
-    </h1>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>User Data {userId}</h1>
+      {error && <p>{error}</p>}
+      {userData ? (
+        <>
+          <pre>{JSON.stringify(userData, null, 2)}</pre>
+          <button onClick={handleScoreUpdate}>Update Score</button>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 };
 
