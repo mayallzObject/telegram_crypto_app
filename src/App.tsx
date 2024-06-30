b81e4ba764de712efcffc1543b311e65c5995c13
Ivaylo Yankov <mayallzobject@gmail.com>
2024-06-30 18:49:33 +0300

    feature/fix-3

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC<{ userId: number | null }> = ({ userId }) => {
         try {
           const userDocRef = doc(
             firestore,
-            'users',
+            'users_telegram',
             userId.toString()
           );
           const userDoc = await getDoc(userDocRef);
