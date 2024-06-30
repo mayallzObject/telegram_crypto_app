9f8176aa43c068bb425931b898b11b499c5fae67
Ivaylo Yankov <mayallzobject@gmail.com>
2024-06-30 18:45:01 +0300

    feature/fix-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,55 @@
-// src/App.tsx
-// import React, { useEffect, useState } from 'react';
-// import { fetchDocumentById } from './firestore/users.store';
-// import { DocumentData } from 'firebase/firestore';
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { firestore } from '../firebaseConfig'; // Adjust the import path as necessary
+import { doc, getDoc } from 'firebase/firestore';
 
 const App: React.FC<{ userId: number | null }> = ({ userId }) => {
+  const [userData, setUserData] = useState<{
+    user_id: string;
+    score_points: number;
+  } | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userId !== null) {
+        try {
+          const userDocRef = doc(
+            firestore,
+            'users',
+            userId.toString()
+          );
+          const userDoc = await getDoc(userDocRef);
+          if (userDoc.exists()) {
+            setUserData(
+              userDoc.data() as {
+                user_id: string;
+                score_points: number;
+              }
+            );
+            console.log('User Data:', userDoc.data());
+          } else {
+            setError('User not found');
+            console.log('User not found');
+          }
+        } catch (err) {
+          setError('Error fetching user data');
+          console.error('Error fetching user data:', err);
+        }
+      }
+    };
+
+    fetchUser();
+  }, [userId]);
+
   return (
     <div>
-      <h1>User Data</h1>
-      <pre>{JSON.stringify(userId, null, 2)}</pre>
+      <h1>User Data {userId}</h1>
+      {error && <p>{error}</p>}
+      {userData ? (
+        <pre>{JSON.stringify(userData, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
